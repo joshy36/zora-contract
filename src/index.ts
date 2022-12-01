008f711177ada4668c453a7fab29aef334bf86b0
Joshua Bender <36bend@gmail.com>
2022-12-01 13:32:50 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { ethers, Wallet } from 'ethers';
+import abi from './ReserveAuctionCoreEth.json' assert { type: 'json' };
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+const { PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+
+interface Artifact {
+  contractName: string;
+  sourceName: string;
+  abi: any;
+  bytecode: string;
+  deployedBytecode: string;
+  linkReferences: object;
+  deployedLinkReferences: object;
+}
+
+const contractAddress = '0x2506D9F5A2b0E1A2619bCCe01CD3e7C289A13163';
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
+);
+const wallet = new Wallet(PRIVATE_KEY, provider);
+const contractAbi = abi as unknown as Artifact;
+
+const contract = new ethers.Contract(
+  contractAddress,
+  contractAbi.abi,
+  provider
+);
+
+const txConfig = {
+  gasLimit: 5000000,
+  maxFeePerGas: ethers.utils.parseUnits('2', 'gwei'),
+  maxPriorityFeePerGas: ethers.utils.parseUnits('2', 'gwei'),
+};
+
+const tx = await contract
+  .connect(wallet)
+  .createAuction(
+    '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
+    11,
+    10,
+    10,
+    '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
+    10,
+    txConfig
+  );
+
+console.log(tx.hash);
